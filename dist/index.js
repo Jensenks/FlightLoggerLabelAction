c5c090efb475264b0d99b53c025e9aa447d0cbd6
Kåre Jensen <jensenks@gmail.com>
2020-05-27 12:48:08 +0200

    Edit yml files

--- dist/index.js
+++ dist/index.js
@@ -3814,60 +3814,6 @@ function run() {
         }
     });
 }
-// async function getLabelGlobs(
-//   client: github.GitHub,
-//   configurationPath: string
-// ): Promise<Map<string, string[]>> {
-//   const configurationContent: string = await fetchContent(
-//     client,
-//     configurationPath
-//   );
-//   // loads (hopefully) a `{[label:string]: string | string[]}`, but is `any`:
-//   const configObject: any = yaml.safeLoad(configurationContent);
-//   // transform `any` => `Map<string,string[]>` or throw if yaml is malformed:
-//   return getLabelGlobMapFromObject(configObject);
-// }
-// async function fetchContent(
-//   client: github.GitHub,
-//   repoPath: string
-// ): Promise<string> {
-//   const response: any = await client.repos.getContents({
-//     owner: github.context.repo.owner,
-//     repo: github.context.repo.repo,
-//     path: repoPath,
-//     ref: github.context.sha
-//   });
-//   return Buffer.from(response.data.content, response.data.encoding).toString();
-// }
-// function getLabelGlobMapFromObject(configObject: any): Map<string, string[]> {
-//   const labelGlobs: Map<string, string[]> = new Map();
-//   for (const label in configObject) {
-//     if (typeof configObject[label] === 'string') {
-//       labelGlobs.set(label, [configObject[label]]);
-//     } else if (configObject[label] instanceof Array) {
-//       labelGlobs.set(label, configObject[label]);
-//     } else {
-//       throw Error(
-//         `found unexpected type for label ${label} (should be string or array of globs)`
-//       );
-//     }
-//   }
-//   return labelGlobs;
-// }
-// function checkGlobs(changedFiles: string[], globs: string[]): boolean {
-//   for (const glob of globs) {
-//     core.debug(` checking pattern ${glob}`);
-//     const matcher = new Minimatch(glob);
-//     for (const changedFile of changedFiles) {
-//       core.debug(` - ${changedFile}`);
-//       if (matcher.match(changedFile)) {
-//         core.debug(` ${changedFile} matches`);
-//         return true;
-//       }
-//     }
-//   }
-//   return false;
-// }
 function addLabels(client, prNumber, labels) {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.issues.addLabels({
